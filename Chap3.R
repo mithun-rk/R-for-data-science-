c35bdbbbad593816edf138f7ff9257ea5d7c51ba
Mithun Radhakrishnan <mithunradhakrishnan@hotmail.com>
2018-02-22 18:13:52 +0530

    more data wrangling functions

--- Chap3.R
+++ Chap3.R
@@ -48,12 +48,103 @@ arrange(flights,year, month,day) #uses set of column names
 # use desc() to column in descending order
 arrange(flights,desc(arr_delay))
 
+## Excercise pending
+
+# Select columns with select()
+select(flights,year,month,day) # specific columns
+select(flights, year:day) # between year and day
+
+# select all columns except between year and day (inclusive)
+select(flights,-(year:day)) # dropped some variables
+# there are various helper functions of select
+# start_with("abc")
+# end_with("abc")
+# contains("abc")
+# matches() # not sure about this 
+# num_range("x", 1:3) matches x1, x2 and x3.
 
 
+# Use rename() which is a variant of select to rename variables
+rename(flights, tail_num = tailnum)
+head(flights)
+View(flights)
 
+# Another variant of select is to use it with everything()
+select(flights,time_hour,air_time,everything()) # basically moves the 
+# 2 variables of interest to the start of df.
 
+#### Excercise pending
 
+## Add new variable with mutate()
+# mutate adds new columns at the end of your dataset
+flights_sml <- select(flights,  year:day,  ends_with("delay"),
+                      distance,  air_time ) 
+head(flights_sml)
 
+# using mutate to create 2 new variable
+(mutate(flights_sml,  gain = arr_delay - dep_delay,
+       speed = distance / air_time * 60)) 
+       
+(mutate(flights_sml,  gain = arr_delay - dep_delay,
+       hours = air_time / 60,  gain_per_hour = gain / hours )) 
+
+# if you only want to keeo the new variables use transmute()
+(transmute(flights,  gain = arr_delay - dep_delay,
+  hours = air_time / 60,  gain_per_hour = gain / hours )) 
+
+# Modular arthmetic
+# %/% will keep only the integer part, the remainder post division is ignored
+transmute(flights,dep_time,hour=dep_time %/% 100,
+          minute = dep_time %/% 100)
+# Cumulative rolling aggregates 
+# cumsum()
+# cumprod()
+# cummin()
+# cummax()
+# cummean()
+
+# Ranking functions 
+y<- c(1,2,2,NA,3,4)
+min_rank(y)
+min_rank(desc(y))
+
+row_number(y)
+dense_rank(y)
+percent_rank(y)
+cume_dist(y)
+
+## Grouped summaries with summarize()
+summarize(flights, delay = mean(dep_delay, na.rm = TRUE)) # not much of use 
+summary(flights$dep_delay)
+
+## a better usage
+by_day <- group_by(flights, year, month, day)
+summarize(by_day, delay = mean(dep_delay, na.rm = TRUE)) 
+## group_by() and summarize() will be used very commonly. 
+
+## Combining multiple operations with pipe
+by_dest <- group_by(flights,dest)
+# here we define a variable by its dest
+delay<- summarize(by_dest,count = n(),
+                  dist = mean(distance, na.rm = TRUE),
+                  delay = mean(arr_delay, na.rm = TRUE))
+# then we define a summary count of dest with avg of dist & delay
+delay <- filter(delay, count > 20, dest != "HNL")
+# now we filtert by count greater than 20 and dest HNL
+
+ggplot(data = delay, mapping = aes(x = dist, y = delay)) +
+  geom_point(aes(size = count), alpha = 1/3) +
+  geom_smooth(se = FALSE)
+
+## %>% called the pipe and can come handy
+delays <- flights %>% group_by(dest) %>% summarise(count=n(),
+          dist = mean(distance,na.rm = TRUE),
+          delay = mean(arr_delay, na.rm = TRUE))%>%
+  filter(count > 20, dest != "HNL")
+## Read %>% as then 
+## group by then summarise then filter
+
+## Missing values 
 
 
 
